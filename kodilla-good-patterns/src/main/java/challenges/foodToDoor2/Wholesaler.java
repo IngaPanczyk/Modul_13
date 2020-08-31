f23a65ea4f3530405fefc696ca79b9afcedab620
IngaPanczyk <ingawaaa@gmail.com>
2020-08-31 18:11:04 +0200

    2020-08-31

--- kodilla-good-patterns/src/main/java/challenges/foodToDoor2/Wholesaler.java
+++ kodilla-good-patterns/src/main/java/challenges/foodToDoor2/Wholesaler.java
@@ -3,13 +3,13 @@ package challenges.foodToDoor2;
 public class Wholesaler {
     public void prepareOrder(Order order) {
 
-        if (order.getShop() == "ExtraFoodShop") {
+        if (order.getShop().equals("ExtraFoodShop")) {
             ExtraFoodShop extraFoodShop = new ExtraFoodShop(order.getItem(), order.getQuantity());
             extraFoodShop.process(order);
-        } else if (order.getShop() == "GlutenFreeShop") {
+        } else if (order.getShop().equals("GlutenFreeShop")) {
             GlutenFreeShop glutenFreeShop = new GlutenFreeShop(order.getItem(), order.getQuantity());
             glutenFreeShop.process(order);
-        } else if (order.getShop() == "HealtyShop") {
+        } else if (order.getShop().equals("HealtyShop")) {
             HealthyShop healthyShop = new HealthyShop(order.getItem(), order.getQuantity());
             healthyShop.process(order);
         } else {
